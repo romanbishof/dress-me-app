9bf0907cb537c806dce15e05e6f1cb81f16150a8
romanbishof <bishofkristina@gmail.com>
2023-07-05 20:41:09 +0300

    filtering component is dynamic

--- src/components/Filtering.jsx
+++ src/components/Filtering.jsx
@@ -6,22 +6,21 @@ const Filtering = ({ filterType }) => {
     (state) => state.ClosetData
   );
 
+  const values = Object.keys(itemsTypesObj)
+    .filter((key) => filterSelection[key])
+    .flatMap((key) => itemsTypesObj[key][filterType + "s"])
+    .filter((value, index, self) => self.indexOf(value) === index);
+
   return (
-    <div className="Filtering flex items-center pl-4  border-gray-200 rounded w-[200px]">
+    <div className="Filtering flex items-center pl-4 border-gray-200 rounded w-[200px]">
       <select
         id={filterType}
         className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
       >
-        <option defaultValue={filterType}>Color</option>
-        {Array.from(
-          new Set(
-            Object.keys(itemsTypesObj)
-              .filter((key) => filterSelection[key])
-              .flatMap((key) => itemsTypesObj[key].colors)
-          )
-        ).map((color) => (
-          <option key={color} value={color}>
-            {color}
+        <option defaultValue={filterType}>{filterType}</option>
+        {values.map((value) => (
+          <option key={value} value={value}>
+            {value}
           </option>
         ))}
       </select>
